99338ce693611b6530bffe28d9c7829c35ef85a1
Prabin Pramanik <thisIsPrabin@users.noreply.github.com>
2016-04-12 06:33:28 +0530

    Client Server communication

---
+++ Client2.java
@@ -0,0 +1,25 @@
+package Networing;
+
+import java.net.*;
+import java.io.*;
+
+public class Client2 {
+	
+	public static void main(String[] args) throws Exception{
+		Socket s = new Socket("localhost",888);
+		DataOutputStream data = new DataOutputStream(s.getOutputStream());
+		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str1, str2;
+		while(!(str2 = kb.readLine()).equals("exit")){
+			data.writeBytes(str2+"\n");
+			str1 = br.readLine();
+			System.out.println(str1);
+		}
+		
+		data.close();
+		br.close();
+		kb.close();
+	}
+}
---
+++ Server2.java
@@ -0,0 +1,25 @@
+package Networing;
+
+import java.net.*;
+import java.io.*;
+
+public class Server2 {
+	
+	public static void main(String[] args) throws Exception{
+		ServerSocket ss = new ServerSocket(888);
+		Socket s = ss.accept();
+		PrintStream pr = new PrintStream(s.getOutputStream());
+		BufferedReader br  = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true){
+			String str1, str2;
+			
+			while((str1 = br.readLine())!= null){
+				System.out.println(str1);
+				str2 = kb.readLine();
+				pr.println(str2);
+			}
+		}
+	}
+}

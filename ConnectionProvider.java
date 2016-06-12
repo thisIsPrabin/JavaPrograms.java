cd60799b72e465b4c9710f0163489055a975d367
Prabin Pramanik <thisIsPrabin@users.noreply.github.com>
2016-06-12 20:15:43 +0530

    Add files via upload

---
+++ ConnectionProvider.java
@@ -0,0 +1,32 @@
+package database;
+
+import java.sql.*;
+
+public class ConnectionProvider {
+	
+	static Connection connection;
+	
+	static {
+		if(connection == null){
+			
+			try {
+				
+				String className = "com.mysql.jdbc.Driver";
+				String url = "jdbc:mysql://localhost:3306/test", userName = "root", pass = "";
+				
+				Class.forName(className);
+				connection = DriverManager.getConnection(url, userName, pass);
+				
+				
+			}catch(Exception e){
+				System.out.println("ERROR: Establishing the connection");
+			}
+		}
+	}
+	
+	
+	public static Connection getConnection(){
+		return connection;
+	}
+
+}

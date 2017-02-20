05d5cca35b282b604410fb85a4787cf1515b12ad
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-20 22:17:28 +0530

    Add files via upload

---
+++ Java-8.7-Nested-if-else-statement/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+	public static void main(String... args) {
+		boolean a = true; 
+		boolean b = false;
+		if(a){
+			if(b){
+				System.out.println("a and b is ture");
+			} else {
+				System.out.println("a is true b is false");
+			}
+		} else {
+			System.out.println("a is false");
+		}
+	}
+}

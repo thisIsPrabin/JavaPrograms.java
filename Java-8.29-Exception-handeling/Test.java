92dd679b0b63402fcfd3d2d3dc8e86d3d7e94bc6
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-20 22:32:22 +0530

    Add files via upload

---
+++ Java-8.29-Exception-handeling/Test.java
@@ -0,0 +1,11 @@
+public class Test {	
+	public static void main(String... args) {
+		try {
+			int a = 10/0;
+			System.out.println(a);
+		} catch(Exception e){
+			System.out.println(e);
+		}
+		
+	}
+}

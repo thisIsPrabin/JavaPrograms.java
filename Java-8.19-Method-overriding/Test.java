a6a4030ed13af1f2aff8ae54f7cf81683aa9d38a
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-20 22:26:15 +0530

    Add files via upload

---
+++ Java-8.19-Method-overriding/Test.java
@@ -0,0 +1,17 @@
+class A {
+	public void show(){
+		System.out.println("A class method");
+	}
+}
+
+public class Test extends A {
+
+	public void show(){
+		System.out.println("Override the A class method");
+	}
+	
+	public static void main(String... args) {
+		Test obj = new Test();
+		obj.show();
+	}
+}

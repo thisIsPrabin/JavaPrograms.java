4fe6be2327c9d3c5a0bfe0f3f5c46e245741a79d
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-20 22:17:12 +0530

    Add files via upload

---
+++ Java-8.6-If-else-statement/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+	public static void main(String... args) {
+		boolean b = false;
+		if(b){
+			System.out.println("b is ture");
+		} else {
+			System.out.println("b is false");
+		}
+	}
+}

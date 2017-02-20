6b36381ae2a1f1a19bd19369d50231f4b0561f2e
Prabin Pramanik <thisisprabin@users.noreply.github.com>
2017-02-20 22:16:09 +0530

    Add files via upload

---
+++ Java-8.3-Variables/Test.java
@@ -0,0 +1,20 @@
+public class Test {
+
+	public static void main(String... args) {
+		int a = 10;
+		float b = 10f;
+		double c = 10d;
+		char d = 'a';
+		byte e = 10;
+		short f = 10;
+		boolean g = true;
+		
+		System.out.println("int - "+a);
+		System.out.println("float - "+b);
+		System.out.println("double - "+c);
+		System.out.println("char - "+d);
+		System.out.println("byte - "+e);
+		System.out.println("short - "+f);
+		System.out.println("boolean - "+g);
+	}
+}
